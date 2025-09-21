af11c64cee428697967bbcbf7045c0060d238cd4
sipl-navneet <154585691+sipl-navneet@users.noreply.github.com>
2025-09-22 01:40:45 +0530

    Updates

--- assets/KeyboardArrowRight-dpFLvVC_.js
+++
@@ -1,1 +0,0 @@
-import{a8 as a,j as o}from"./index-CbRCKR0p.js";const t=a(o.jsx("path",{d:"M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"})),s=a(o.jsx("path",{d:"M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"}));export{t as K,s as a};
